489afc35e2672675b0f6693a9252fc59f09fdeb7
CezarPetreanu <cezar.petreanu02@e-uvt.ro>
2023-11-05 01:13:42 +0200

    navbar

--- bookworm/frontend/src/app/app.module.ts
+++ bookworm/frontend/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExempluComponent } from './exemplu/exemplu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExempluComponent
-  ],
+  declarations: [AppComponent, ExempluComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
