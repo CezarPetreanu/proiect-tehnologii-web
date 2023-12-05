894d1911dd3add6aaab47d407abaff55ab9145b7
CezarPetreanu <cezar.petreanu02@e-uvt.ro>
2023-12-05 19:41:00 +0200

    reverted

--- bookworm/frontend/src/main.ts
+++ bookworm/frontend/src/main.ts
@@ -2,12 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ThemeService } from './app/app-logic/theme.service';
 import { AppModule } from './app/app.module';
 
-// Create an instance of ThemeService
 const themeService = new ThemeService();
 
-// Subscribe to the darkMode$ observable
 themeService.darkMode$.subscribe((darkMode) => {
-  // Update the body class based on the dark mode state
   document.body.classList.toggle('dark-mode', darkMode);
 });
 
